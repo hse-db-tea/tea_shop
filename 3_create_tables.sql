aa72836b8a9cf4dae8269fc776818e72dead1d2b
sergfedchin <112500418+sergfedchin@users.noreply.github.com>
2023-12-19 14:54:34 +0300

    fix 3_create_tables.sql
    upper case added, foreign key constraint removed on tables, referencing product_id

--- 3_create_tables.sql
+++ 3_create_tables.sql
@@ -1,13 +1,13 @@
-drop SCHEMA IF EXISTS project CASCADE;
-create SCHEMA project;
-set search_path = project, public;
+DROP SCHEMA IF EXISTS project CASCADE;
+CREATE SCHEMA project;
+SET search_path = project, PUBLIC;
 
 DROP TABLE IF EXISTS customer CASCADE;
 CREATE TABLE customer (
 	customer_id Integer, 
   	first_name varchar(100),
-    last_name varchar(100),
-  	phone_number varchar(15) check(regexp_match(phone_number, '^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$') is not null),
+    	last_name varchar(100),
+  	phone_number varchar(15) CHECK(regexp_match(phone_number, '^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$') IS NOT NULL),
   	PRIMARY KEY(customer_id)
 );
 
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS delivery_service CASCADE;
 CREATE TABLE delivery_service (
 	delivery_service_id Integer, 
   	"name" varchar(100),
-    contact_number varchar(15) check(regexp_match(contact_number, '^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$') is not null),
+	contact_number varchar(15) CHECK(regexp_match(contact_number, '^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$') IS NOT NULL),
   	email varchar(100),
   	address text,
   	PRIMARY KEY(delivery_service_id)
@@ -29,21 +29,21 @@ CREATE TABLE warehouse (
 
 DROP TABLE IF EXISTS product CASCADE;
 CREATE TABLE product(
-	product_id Integer UNIQUE NOT NULL,
+	product_id Integer NOT NULL,
   	valid_from TIMESTAMP,
   	valid_to TIMESTAMP,
   	"name" varchar(100),
   	contents text,
-  	price Integer check (price >= 0),
+  	price Integer CHECK(price >= 0),
   	PRIMARY KEY (product_id, valid_from)
 );
 
-drop TABLE IF EXISTS "order" CASCADE;
+DROP TABLE IF EXISTS "order" CASCADE;
 CREATE TABLE "order" (
 	order_id Integer,
-  	customer_id Integer REFERENCES customer(customer_id),
+  	customer_id Integer,
   	delivery_service_id Integer REFERENCES delivery_service(delivery_service_id),
-  	status varchar(20) check(status IN ('Принят', 'В сборке', 'Ожидает доставки', 'Передан курьеру', 'Доставлен')),
+  	status varchar(20) CHECK(status IN ('Принят', 'В сборке', 'Ожидает доставки', 'Передан курьеру', 'Доставлен')),
   	"date" date,
   	"cost" Integer,
   	address text,
@@ -55,7 +55,7 @@ CREATE TABLE "order" (
 DROP TABLE IF EXISTS order_x_product CASCADE;
 CREATE TABLE order_x_product(
 	order_id Integer REFERENCES "order"(order_id),
-  	product_id Integer REFERENCES product(product_id),
+  	product_id Integer,
   	warehouse_id Integer REFERENCES warehouse(warehouse_id),
   	quantity INTEGER CHECK(quantity > 0),
   	PRIMARY KEY(order_id, product_id, warehouse_id)
@@ -63,7 +63,7 @@ CREATE TABLE order_x_product(
 
 DROP TABLE IF EXISTS product_x_warehouse CASCADE;
 CREATE TABLE product_x_warehouse(
-  	product_id Integer REFERENCES product(product_id),
+  	product_id Integer,
   	warehouse_id Integer REFERENCES warehouse(warehouse_id),
   	valid_from TIMESTAMP,
  	valid_to TIMESTAMP,
