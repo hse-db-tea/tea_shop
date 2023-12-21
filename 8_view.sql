3d7e0f897d098d69f8f9ef9d10f3b36f9dd3d631
Artem Batygin <aa.batygin@gmail.com>
2023-12-21 20:49:02 +0300

    Add project_view schema

--- 8_view.sql
+++ 8_view.sql
@@ -1,13 +1,16 @@
-set search_path = project, public;
+drop schema if exists project_views cascade;
+create schema project_views;
+
+set search_path = project_views, public;
 
 -- доступное количество товаров по всем складам
 
-drop view if exists product_total_quantity;
+drop view if exists product_total_quantity cascade;
 create or replace view product_total_quantity as (
 	select 
 		product_id, 
 		sum(quantity) as total_quantity
-	from product_x_warehouse
+	from project.product_x_warehouse
 	where current_timestamp between valid_from and valid_to
 	group by product_id
 	order by sum(quantity) desc
@@ -18,13 +21,13 @@ from product_total_quantity;
 
 -- востробованность каждого склада по количеству запросов и товаров
 
-drop view if exists warehouse_relevance;
+drop view if exists warehouse_relevance cascade;
 create or replace view warehouse_relevance as (
 	select distinct 
 		warehouse_id,
 		count(*) over (partition by warehouse_id) as orders_amount,
 		sum(quantity) over (partition by warehouse_id) as products_amount
-	from product_x_warehouse
+	from project.product_x_warehouse
 	order by products_amount desc
 );
 
@@ -33,7 +36,7 @@ from warehouse_relevance;
 
 -- статичтика по дням за последние 30 дней
 
-drop view if exists statistics_by_month;
+drop view if exists statistics_by_month cascade;
 create or replace view statistics_by_month as (
 	with relevant_order_data as (
 		with relevant_orders as (
@@ -41,7 +44,7 @@ create or replace view statistics_by_month as (
 				order_id,
 				"date",
 				"cost"
-			from "order"
+			from project."order"
 			where "date" >= current_timestamp - interval '30 days'
 		)
 		select 
@@ -51,7 +54,7 @@ create or replace view statistics_by_month as (
 			op.product_id,
 			op.quantity
 		from relevant_orders ro
-		inner join order_x_product op
+		inner join project.order_x_product op
 		on ro.order_id = op.order_id
 	)
 	select distinct
@@ -60,7 +63,7 @@ create or replace view statistics_by_month as (
 		sum(rod."cost") as total_revenue,
 		count(rod.product_id * rod.quantity) as product_amount
 	from relevant_order_data rod
-	inner join product p
+	inner join project.product p
 	on p.product_id = rod.product_id
 	where rod."date" between p.valid_from and p.valid_to
 	group by rod."date"
